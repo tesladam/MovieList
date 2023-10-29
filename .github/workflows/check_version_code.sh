607f6677c3c48c34e888b34d4e7540db6abe35e1
Hakan Yılmaz <hkn1899@gmail.com>
2023-10-29 03:25:19 +0300

    ci/cd güncellendi

--- .github/workflows/check_version_code.sh
+++ .github/workflows/check_version_code.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # check_version_code.sh
-PREV_VERSION_CODE=$(git show HEAD~1:app/build.gradle | grep versionCode | awk '{print $2}')
+
+# Eğer HEAD~1 geçersizse, script false döndürerek işlemlerin geri kalanını atlar
+PREV_VERSION_CODE=$(git show HEAD~1:app/build.gradle | grep versionCode | awk '{print $2}' || echo "")
 CURR_VERSION_CODE=$(grep versionCode app/build.gradle | awk '{print $2}')
 
-if [ "$PREV_VERSION_CODE" != "$CURR_VERSION_CODE" ]; then
-  echo "versionCode changed. Proceeding with distribution..."
-  exit 0
+if [ "$PREV_VERSION_CODE" != "$CURR_VERSION_CODE" ] && [ ! -z "$PREV_VERSION_CODE" ]; then
+  echo "true"
 else
-  echo "versionCode did not change. Skipping distribution..."
-  exit 1
+  echo "false"
 fi
