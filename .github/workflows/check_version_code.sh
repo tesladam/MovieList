f8ebbbd5c9cf56ecddf4e0510bd4441b2c90e807
Hakan Yılmaz <hkn1899@gmail.com>
2023-10-29 04:21:41 +0300

    ci/cd güncellendi

--- .github/workflows/check_version_code.sh
+++ .github/workflows/check_version_code.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 # check_version_code.sh
 
-# Check if this is the first commit by checking the number of commits
-COMMIT_COUNT=$(git rev-list --count HEAD)
-
-if [ "$COMMIT_COUNT" -eq 1 ]; then
-  echo "::set-env name=version-changed::false"
-  echo "First commit, skipping distribution..."
-  exit 0
-fi
-
-# Compare the versionCode between the last commit and the current commit
-PREV_VERSION_CODE=$(git show HEAD^:app/build.gradle | grep versionCode | awk '{print $2}')
+PREV_VERSION_CODE=$(git log -n 1 --pretty=format:%h --skip=1 -- app/build.gradle | xargs -I {} git show {}:app/build.gradle | grep versionCode | awk '{print $2}')
 CURR_VERSION_CODE=$(grep versionCode app/build.gradle | awk '{print $2}')
 
 if [ "$PREV_VERSION_CODE" != "$CURR_VERSION_CODE" ]; then
-  echo "::set-env name=version-changed::true"
-  echo "versionCode changed. Proceeding with distribution..."
+  echo "version-changed=true" >> $GITHUB_ENV
 else
-  echo "::set-env name=version-changed::false"
-  echo "versionCode did not change. Skipping distribution..."
+  echo "version-changed=false" >> $GITHUB_ENV
 fi
