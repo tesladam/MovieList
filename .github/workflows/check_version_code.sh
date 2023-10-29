cb71ec0a9eb64206510d60b2cd33293c493202e0
Hakan Yılmaz <hkn1899@gmail.com>
2023-10-29 04:03:34 +0300

    Update check_version_code.sh

--- .github/workflows/check_version_code.sh
+++ .github/workflows/check_version_code.sh
@@ -1,26 +1,19 @@
 #!/bin/bash
 # check_version_code.sh
 
-# Check if this is the first commit
-if [ $(git rev-list --count HEAD) -eq 1 ]; then
-  echo "First commit, proceeding with distribution..."
-  echo 'true' > $GITHUB_ENV
-  exit 0
-fi
-
 # Get a list of files changed between the current commit and the previous commit
-CHANGED_FILES=$(git diff HEAD~1 --name-only)
+CHANGED_FILES=$(git diff HEAD^ HEAD --name-only)
 
 # Check if app/build.gradle is in the list of changed files
 if echo "$CHANGED_FILES" | grep -q "app/build.gradle"; then
-  # app/build.gradle has changed, now check if versionCode has changed
-  VERSION_CODE_CHANGED=$(git diff HEAD~1 -- app/build.gradle | grep versionCode)
-  if [ -n "$VERSION_CODE_CHANGED" ]; then
-    echo 'true' > $GITHUB_ENV
-    exit 0
-  fi
+    # app/build.gradle has changed, now check if versionCode has changed
+    VERSION_CODE_CHANGED=$(git diff HEAD^ HEAD -- app/build.gradle | grep versionCode)
+    if [ -n "$VERSION_CODE_CHANGED" ]; then
+        echo "::set-output name=version-changed::true"
+        exit 0
+    fi
 fi
 
 # Either app/build.gradle was not changed, or versionCode was not changed
-echo 'false' > $GITHUB_ENV
+echo "::set-output name=version-changed::false"
 exit 0
