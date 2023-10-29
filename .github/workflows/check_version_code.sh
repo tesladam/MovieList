a0a266db76714c5587e7862638deb6d27d41b9c6
Hakan Yılmaz <hkn1899@gmail.com>
2023-10-29 03:55:49 +0300

    ci/cd güncellendi

--- .github/workflows/check_version_code.sh
+++ .github/workflows/check_version_code.sh
@@ -1,31 +1,24 @@
 #!/bin/bash
-# check_version_code.sh
 
-# Commit sayısını elde edin
-COMMIT_COUNT=$(git rev-list --count HEAD)
+# Eğer HEAD referansı yoksa (yani henüz commit yapılmamışsa), scripti çıkış yap
+if ! git rev-parse HEAD > /dev/null 2>&1; then
+    echo "::set-output name=version-changed::false"
+    exit 0
+fi
 
-# Eğer commit sayısı 1'den büyükse, HEAD~1 referansını kullanabilirsiniz
-if [ "$COMMIT_COUNT" -gt 1 ]; then
-  # Son committe değişiklik yapılan dosyaları elde edin
-  CHANGED_FILES=$(git diff HEAD~1 --name-only)
+# En son commit ile bir önceki commit arasındaki farkları al
+CHANGED_FILES=$(git diff HEAD^ HEAD --name-only)
 
-  # Eğer app/build.gradle dosyası değiştirilmişse, versionCode'un değişip değişmediğini kontrol edin
-  if echo "$CHANGED_FILES" | grep -q "app/build.gradle"; then
-    PREV_VERSION_CODE=$(git show HEAD~1:app/build.gradle | grep versionCode | awk '{print $2}')
+# Eğer app/build.gradle dosyası değiştirilmişse, versionCode'un değişip değişmediğini kontrol et
+if echo "$CHANGED_FILES" | grep -q "app/build.gradle"; then
+    PREV_VERSION_CODE=$(git show HEAD^:app/build.gradle | grep versionCode | awk '{print $2}')
     CURR_VERSION_CODE=$(grep versionCode app/build.gradle | awk '{print $2}')
 
     if [ "$PREV_VERSION_CODE" != "$CURR_VERSION_CODE" ]; then
-      echo "versionCode changed. Proceeding with distribution..."
-      exit 0
+        echo "::set-output name=version-changed::true"
     else
-      echo "versionCode did not change. Skipping distribution..."
-      exit 1
+        echo "::set-output name=version-changed::false"
     fi
-  else
-    echo "app/build.gradle did not change. Skipping distribution..."
-    exit 1
-  fi
 else
-  echo "Not enough commit history. Skipping distribution..."
-  exit 1
+    echo "::set-output name=version-changed::false"
 fi
